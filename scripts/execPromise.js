0ff4d1ee92de33928732a6d853d4ab8602cd4c1d
蔡柄泓 <caibinghong@tuzhanai.com>
2021-08-10 14:03:43 +0800

    feat(debug): 添加command log

--- scripts/execPromise.js
+++ scripts/execPromise.js
@@ -1,8 +1,16 @@
 const shell = require('shelljs')
+const {pDebug} = require('./print')
 
 const execPromise = async (command) => {
   return new Promise((resolve, reject) => {
     shell.exec(command, (code, stdout, stderr) => {
+      pDebug(`------- log -------`)
+      pDebug('command', command)
+      pDebug('code', code)
+      pDebug('stdout', stdout)
+      pDebug('stderr', stderr)
+      pDebug('\n')
+      
       if (code === 0) {
         return resolve(stdout)
       } else {
--- scripts/print.js
+++ scripts/print.js
@@ -1,14 +1,16 @@
 const chalk = require("chalk")
 
-const pSuccess = msg => console.log(chalk.green(msg))
+const pSuccess = (...msg) => console.log(chalk.green(...msg))
 
-const pError = msg => console.error(chalk.red(msg))
+const pError = (...msg) => console.error(chalk.red(...msg))
 
-const pLog = msg => console.error(chalk.white(msg))
+const pLog = (...msg) => console.error(chalk.white(...msg))
 
+const pDebug = (...msg) => console.error(chalk.blueBright(...msg))
 
 module.exports = {
   pSuccess,
   pError,
-  pLog
+  pLog,
+  pDebug
 }
